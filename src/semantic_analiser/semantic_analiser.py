cd357f622bd7e868dbca82f208acc36a979d912e
Matheus Mortatti <matheusmortatti@gmail.com>
2018-04-04 16:56:09 -0300

    exemplo de leitura da AST

--- src/semantic_analiser/semantic_analiser.py
+++ src/semantic_analiser/semantic_analiser.py
@@ -1,4 +1,14 @@
-from ..AST import Node
+from ..AST import AST
+from ..AST.AST import Node
 
 def analise( head: Node ):
-    return "analised stuff"
+    print(head.type)
+    for child in head.next:
+        analise(child)
+
+if __name__ == "__main__":
+    node = AST.ProgramNode(Node())
+    node.addNext(AST.TokenNode(Node()))
+    node.addNext(Node())
+    node.addNext(Node())
+    analise(node)
