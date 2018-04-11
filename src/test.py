cac8282f3f0207fa3affd9ec45caf620458738e5
Thiago Silva de Farias <ra148077@students.ic.unicamp.br>
2018-04-11 19:30:42 -0300

    Scripts, Exceptions e mudanças de diretórios

--- src/test.py
+++ src/test.py
@@ -1,12 +1,13 @@
 import os
+import compiler.parser.apollo_yacc as parser
+import compiler.lexer.test_lexer as testlexer
 
-
-def run_tests():
-    dirs = os.listdir()
-    for dir in dirs:
-        if os.path.isdir(dir):
-            os.system('python3 ' + dir + '/test_' + dir + '.py')
-
+#def run_tests():
+    #dirs = os.listdir()
+    #dirs = ['lexer']
+    #for dir in dirs:
+        #if os.path.isdir(dir):
+    #os.system('python3 compiler/lexer/test_lexer.py')
 
 if __name__ == '__main__':
-    run_tests()
+    testlexer.run()
