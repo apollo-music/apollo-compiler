c111325cba5a88724f3507d1f78a948c86bb8074
Henrique <henrique.facioli@gmail.com>
2018-04-17 19:53:17 -0300

    Starting unique command to generate mid file

--- src/run.sh
+++ src/run.sh
@@ -1,1 +1,5 @@
-python3 start.py test.apollo
+#!/bin/bash
+python3 start.py $1
+python2 intermediate.py
+# Precisa achar o arquivo .mid gerado pelo intermediate e converter para mp3 com o comando abaixo
+# timidity my_midi_file.mid -Ow -o - | lame - -b 64 my_converted_midi.mp3
