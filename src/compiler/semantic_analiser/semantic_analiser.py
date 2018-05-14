757b970d16fdfa6a37c3b8947a8485797b9fa94a
Henrique <henrique.facioli@gmail.com>
2018-05-13 23:58:31 -0300

    Scope checking ok

--- src/compiler/semantic_analiser/semantic_analiser.py
+++ src/compiler/semantic_analiser/semantic_analiser.py
@@ -1,16 +1,16 @@
 ## Apollo music gen semantic analysiss
 # This semantic analysis do:
 # - Scope checkin: Variables are declared before using it
-#    - Checks variables
+#    - Checks variables (changes scope on labels, repeats and plays)
 #    - Checks amp, dur and instr
 #    - MISSING REPEAT						- TODO
 # - Valid var use: Check if a var is used an inalid place
 #    - Cannot use ouch = [3,4] e play: [1, (2, ouch), 5]
+#    - Valid vars on amp, dur (missing on tone)
 # - Operation on riht types
 #    - Length of types are wrong			- TODO
 #    - Types are wrong   					- TODO
-## Testes que estão sendo feitos
-# - Passar variável no amp, dur, instr  - NOT WORKING
+
 import sys
 from ..AST import AST
 from ..AST.AST import addToClass
@@ -111,18 +111,20 @@ def analise(self):
 	return self
 
 # EntryNode (First node of program)
-# - 'program2 : START NEWLINE program END NEWLINE' | AST.EntryNode(p[3])
+# 'program2 : START NEWLINE program END NEWLINE' -> AST.EntryNode(p[3])
 @addToClass(AST.EntryNode)
 def analise(self):
 	print('DEBUG: ' + str(self))
-	pushScope(Scope('global'), 0) # Global scope has scope 0
+	# Global scope has scope 0
+	pushScope(Scope('global'), 0)
 	c = self.children[0].analise()
-	# print('DEBUG: ' + str(c))
-	return c
+	
+	# Maybe do some analysis on c and check if its ok (True) or not (False)
+	return True
 
 # ProgramNode (generic)
-# - 'program : statement NEWLINE' | AST.ProgramNode(p[1])
-# - 'program : statement NEWLINE program' | AST.ProgramNode([p[1]] + [p[3]])
+# 'program : statement NEWLINE' -> AST.ProgramNode(p[1])
+# 'program : statement NEWLINE program' -> AST.ProgramNode([p[1]] + [p[3]])
 # '''statement : command | param | assignation | loop'''
 @addToClass(AST.ProgramNode)
 def analise(self):
@@ -140,31 +142,87 @@ def analise(self):
 	return statement
 
 # AmpNode
-# - 'param : AMP TWOPOINTS INT' | AST.AmpNode([AST.TokenNode(p[3])])
+# 'param : AMP TWOPOINTS INT | AMP TWOPOINTS ID ' -> AST.AmpNode([AST.TokenNode(p[3])])
 @addToClass(AST.AmpNode)
 def analise(self):
 	amp = self.children[0].analise()
-	insertSymbol('AMP', amp)
-	return amp
+
+	# Check if its a var
+	if type(amp) is str:
+		# As its a var, it can be: not in scope, a int and valid value or a invalid value
+		var_val = findSymbol(amp)
+		if not var_val:
+			print("Error: %s used but never was defined" % (amp))
+			sys.exit(1)
+		elif type(var_val) is int or len(var_val) == 1:
+			insertSymbol('AMP', var_val)
+			return var_val
+		else:
+			print("Error: Invalid type used on amp: %s" % (amp))
+			sys.exit(1)
+	else:
+		# If its not a var, add it on scope directly
+		insertSymbol('AMP', amp)
+		return amp
 
 # DurNode
-# - 'param : DUR TWOPOINTS INT' | AST.DurNode([AST.TokenNode(p[3])])
+# 'param : DUR TWOPOINTS INT | DUR TWOPOINTS ID' -> AST.DurNode([AST.TokenNode(p[3])])
 @addToClass(AST.DurNode)
 def analise(self):
 	dur = self.children[0].analise()
-	insertSymbol('DUR', dur)
-	return dur
+	# Check if its a var
+	if type(dur) is str:
+		# As its a var, it can be: not in scope, a int and valid value or a invalid value
+		var_val = findSymbol(dur)
+		if not var_val:
+			print("Error: %s used but never was defined" % (dur))
+			sys.exit(1)
+		elif type(var_val) is int or len(var_val) == 1:
+			insertSymbol('DUR', var_val)
+			return var_val
+		else:
+			print("Error: Invalid type used on dur: %s" % (dur))
+			sys.exit(1)
+	else:
+		insertSymbol('DUR', dur)
+		return dur
 
 # InstrNode
-# - 'param : INSTR TWOPOINTS INT' | AST.InstNode([AST.TokenNode(p[3])])
+# 'param : INSTR TWOPOINTS INT' -> AST.InstNode([AST.TokenNode(p[3])])
 @addToClass(AST.InstrNode)
 def analise(self):
 	ins = self.children[0].analise()
 	insertSymbol('INSTR', ins)
 	return ins
 
+# ToneNode
+# 'param : TONE TWOPOINTS INT | TONE TWOPOINTS ID' -> AST.ToneNode([AST.TokenNode(p[3])])
+@addToClass(AST.ToneNode)
+def analise(self):
+	tone = self.children[0].analise()
+
+	## Missing type on var receive verification
+	insertSymbol('TONE', tone)
+	return tone
+
+# CallNode
+# 'param : CALL TWOPOINTS ID' -> AST.CallNode([AST.TokenNode(p[3])])
+@addToClass(AST.CallNode)
+def analise(self):
+	# This will return the name of the call
+	call = self.children[0].analise()
+
+	# Now, needs to find the scope on the stack
+	var_val = findSymbol(call)
+	if not var_val:
+		print("Error: Calling %d is invalid" % (call))
+		sys.exit(1)
+	## Must check if its a valid call
+
+	return var_val
+
 # CommandNode
-# - ' : command COMMA param' | AST.CommandNode([p[3], p[1]])
+# 'command: command COMMA param' -> AST.CommandNode([p[3], p[1]])
 @addToClass(AST.CommandNode)
 def analise(self):
 	pushScope(Scope('comand_' + str(len(AST.ScopeStack))))
@@ -178,7 +236,7 @@ def analise(self):
 	return command + param
 
 # PlayNode
-# 'command : PLAY TWOPOINTS playcontent' | AST.PlayNode([p[3]])
+# 'command : PLAY TWOPOINTS playcontent' -> AST.PlayNode([p[3]])
 @addToClass(AST.PlayNode)
 def analise(self):
 	# Check if amp, dur, and instruments are declared on the scope
@@ -200,9 +258,9 @@ def analise(self):
 	return seqsound
 
 # PlaycontentNode
-# 'playcontent : LBRACKET seqexp RBRACKET' | AST.PlaycontentNode([p[2]])
-# 'playcontent : ID'  | AST.PlaycontentNode(AST.TokenNode(p[1]))
-# 'playcontent : acc' | AST.PlaycontentNode(p[1])
+# 'playcontent : LBRACKET seqexp RBRACKET' -> AST.PlaycontentNode([p[2]])
+# 'playcontent : ID'  -> AST.PlaycontentNode(AST.TokenNode(p[1]))
+# 'playcontent : acc' -> AST.PlaycontentNode(p[1])
 @addToClass(AST.PlaycontentNode)
 def analise(self):
 	play_val = self.children[0].analise()
@@ -212,14 +270,14 @@ def analise(self):
 		var_val = findSymbol(play_val)
 		if not var_val:
 			print("Error: %s used but never was defined" % (play_val))
-			# Should break the program? idn
+			sys.exit(1)
 		else:
 			return var_val
 	
 	return play_val
 
 # VarNode
-# 'assignation : VAR ID TWOPOINTS exp' | AST.VarNode([AST.TokenNode(p[2]), p[4]])
+# 'assignation : VAR ID TWOPOINTS exp' -> AST.VarNode([AST.TokenNode(p[2]), p[4]])
 @addToClass(AST.VarNode)
 def analise(self):
 	ID = self.children[0].analise()
@@ -230,7 +288,7 @@ def analise(self):
 # ExpressionNode
 # - 'exp : LBRACKET seqsound RBRACKET rec_op' |  AST.ExpressionNode([p[2], p[4]])
 # - 'exp : nota rec_op'| AST.ExpressionNode([p[1], p[2]])
-# - 'exp : acc rec_op' | AST.ExpressionNode([p[1], p[2]])
+# - 'exp : acc rec_op' -> AST.ExpressionNode([p[1], p[2]])
 @addToClass(AST.ExpressionNode)
 def analise(self):
 	left = self.children[0].analise()
@@ -240,36 +298,35 @@ def analise(self):
 		var_val = findSymbol(left)
 		if not var_val:
 			print("Error: %s used but never was defined" % (left))
-			# Should break the program? idn
+			sys.exit(1)
 		else:
 			return var_val
 
 	rec_op = self.children[1].analise()
 	if not rec_op:
+		if type(left) is not list:
+			return [left]
 		return left
 	# print('DEBUG: ExpressionNode rec_op: ' + str(rec_op))
 
 	return [left + rec_op]
 
 # SeqexpNode
-# 'seqexp : exp COMMA seqexp' | AST.SeqexpNode([p[1], p[3]])
-# 'seqexp : exp' | AST.SeqexpNode(p[1])
+# 'seqexp : exp COMMA seqexp' -> AST.SeqexpNode([p[1], p[3]])
+# 'seqexp : exp' -> AST.SeqexpNode(p[1])
 @addToClass(AST.SeqexpNode)
 def analise(self):
+	### THIS MAY NOT BE WORKING
 	exp = self.children[0].analise()
-
-	# print('DEBUG exp ' + str(exp))
-
 	if len(self.children) > 1:
 		segexp = self.children[1].analise()
-		# print('DEBUG segexp ' + str(segexp))
-		return [exp, segexp]
+		return exp + segexp
 
 	return exp
 
 # OpNode
 # 'rec_op : SUM exp' |  AST.OpNode(p[1], [p[2]])
-# 'rec_op : MINUS exp' | AST.OpNode(p[1], [p[2]])
+# 'rec_op : MINUS exp' -> AST.OpNode(p[1], [p[2]])
 # 'rec_op : AMPERSAND exp'
 @addToClass(AST.OpNode)
 def analise(self):
@@ -279,10 +336,9 @@ def analise(self):
 	val = self.children[0].analise()
 	return [op, val]
 
-
 # SeqSoundNode
-# 'seqsound : sound COMMA seqsound' | AST.SeqsoundNode([p[1], p[3]])
-# 'seqsound : sound' | AST.SeqsoundNode(p[1])
+# 'seqsound : sound COMMA seqsound' -> AST.SeqsoundNode([p[1], p[3]])
+# 'seqsound : sound' -> AST.SeqsoundNode(p[1])
 @addToClass(AST.SeqsoundNode)
 def analise(self):
 	if len(self.children) > 1:
@@ -294,7 +350,7 @@ def analise(self):
 		return [sound]
 
 # SoundNode
-# 	'''sound : acc | nota''' | AST.SoundNode(p[1])
+# 	'''sound : acc | nota''' -> AST.SoundNode(p[1])
 @addToClass(AST.SoundNode)
 def analise(self):
 	acc_or_nota = self.children[0].analise()
@@ -311,7 +367,7 @@ def analise(self):
 	return acc_or_nota
 
 # AccNode
-# 'acc : LPAREN seqnotas RPAREN' | AST.AccNode([p[2]])
+# 'acc : LPAREN seqnotas RPAREN' -> AST.AccNode([p[2]])
 @addToClass(AST.AccNode)
 def analise(self):
 	seq_notas = self.children[0].analise()
@@ -326,8 +382,8 @@ def analise(self):
 	return tuple(seq_notas)
 
 # SeqNotasNode
-# 'seqnotas : nota' | AST.SeqNotasNode([p[1]])
-# 'seqnotas : nota COMMA seqnotas' | AST.SeqNotasNode([p[1], p[3]])
+# 'seqnotas : nota' -> AST.SeqNotasNode([p[1]])
+# 'seqnotas : nota COMMA seqnotas' -> AST.SeqNotasNode([p[1], p[3]])
 @addToClass(AST.SeqNotasNode)
 def analise(self):
 	nota = self.children[0].analise()
@@ -337,7 +393,7 @@ def analise(self):
 		var_val = findSymbol(nota)
 		if not var_val:
 			print("Error: %s used but never was defined" % (nota))
-			# Should break the program? idn
+			sys.exit(1)
 		else:
 			nota = var_val
 
@@ -354,13 +410,34 @@ def analise(self):
 	return self.tok
 
 # RepeatNode
-# 'loop : REPEAT INT TWOPOINTS NEWLINE program ENDREPEAT' | AST.RepeatNode([AST.TokenNode(p[2]), p[5]])
+# 'loop : REPEAT INT TWOPOINTS NEWLINE program ENDREPEAT' -> AST.RepeatNode([AST.TokenNode(p[2]), p[5]])
 @addToClass(AST.RepeatNode)
 def analise(self):
+	pushScope(Scope('repeat__' + str(ID)))
+	# Gets the number of repretition (Not needed)
+	REP = self.children[0].analise()
+	# Analise if the program if OK
+	program =  self.children[1].analise()
+	# Now, remove the scope
+	popScope()
 	return self
 
+
+# RepeatNode
+# 'label : SEQUENCE ID TWOPOINTS NEWLINE program ENDSEQUENCE' -> AST.LabelNode([AST.TokenNode(p[2]), p[5]])
+@addToClass(AST.LabelNode)
+def analise(self):
+	pushScope(Scope('label__' + str(ID)))
+	# Gets the ID
+	ID = self.children[0].analise()
+	# Analise if the program if OK
+	program =  self.children[1].analise()
+	# If it is, add to scope so if the code can find it
+	insertSymbol(ID, program)
+	return True
+
 # EmptyNode
-# 'rec_op : ' | AST.EmptyNode()
+# 'rec_op : ' -> AST.EmptyNode()
 @addToClass(AST.EmptyNode)
 def analise(self):
 	return False
@@ -368,16 +445,24 @@ def analise(self):
 
 ## This is the function to execute the semantic analiser
 def run(ast):
-	ast.analise()
-	#print("Semantic Analisys Completed...")
+	'''
+		Execute the semantic analisis on ast argument
+	'''
+	return ast.analise()
 
 
-def test():
+def test(filename=None):
+	'''
+		This function test the sem analise on the file from argv[1] or from filename (if any)
+	'''
 	import sys
 	import os
 	from compiler.parser import apollo_yacc
 
-	f = open(sys.argv[1], 'r')
+	if filename is None:
+		f = open(sys.argv[1], 'r')
+	else:
+		f = open(filename, 'r')
 	prog = f.read()
 	f.close()
 
