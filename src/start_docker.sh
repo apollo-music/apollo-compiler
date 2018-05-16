527caa3849e3e718a25cb0850bcdbb9070721612
Matheus Jun Ota <ra138889@students.ic.unicamp.br>
2018-05-16 13:06:55 -0300

    Update start_docker.sh

--- src/start_docker.sh
+++ src/start_docker.sh
@@ -1,5 +1,1 @@
-service docker stop
-thin_check /home/docker/devicemapper/devicemapper/metadata
-thin_check --clear-needs-check-flag  /home/docker/devicemapper/devicemapper/metadata
-service docker start
 docker run -it --rm --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v=$(pwd)/..:$(pwd)/.. -w=$(pwd) apollo bash
