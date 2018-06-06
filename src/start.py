4189fd14f6b1ecfedfa486bfda817d11dfdacdef
Matheus Jun Ota <matheusota@gmail.com>
2018-06-06 17:01:45 -0300

    corrigido testes semantic analysis

--- src/start.py
+++ src/start.py
@@ -4,7 +4,7 @@ import compiler.semantic_analiser.semantic_analiser as sm
 import compiler.codegen.apollo_codegen as codegen
 
 # For parser execuion just uncomment parser.run and execute ./start.py filein
-# parser.run()
+# sparser.run()
 # For sm execuion just uncomment parser.run and execute ./start.py filein
 # sm.debug()
 # For usage of codegen, uncomment codege.run() and execute ./start.py filein
